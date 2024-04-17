243ffc2392898a284f21f283397f03c41e3c31ba
Yashraj Zope <yashajzope143@gmail.com>
2024-04-17 10:48:15 +0530

    update main

---
+++ pattern22.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n;
+ cin >> n ;
+ int row = 1;
+ while (row <= n )
+ {
+    int space = row - 1;
+    while (space)
+    {
+        cout << " ";
+        space -= 1;
+    }
+    int col = 1;
+    int start = n - row +1;
+    while (col <= start)
+    {
+        cout<< "* ";
+        col +=1;
+    }
+    cout << endl;
+    row +=1;
+    
+ }
+}
